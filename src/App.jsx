09bcb554784d75d55be242c437e31859f69d01f4
Sreethika R <sreethikaradhakrishnan03@gmail.com>
2026-09-05 14:05:44 +0530

    Installed react-router-dom package to resolve import error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import SidebarNav from './components/SidebarNav'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import AnimatedSlide from './components/AnimatedSlide'
 import Hero from './components/Hero'
 import About from './components/About'
-import Journey from './components/Journey'
-import Skills from './components/Skills'
 import Strengths from './components/Strengths'
-import Projects from './components/Projects'
-import Achievements from './components/Achievements'
-import Beyond from './components/Beyond'
-import Goals from './components/Goals'
 import Contact from './components/Contact'
+// ... import your other components as well
 
 function App() {
+  const location = useLocation()
+
   return (
-    <div className="portfolio-container">
-      <SidebarNav />
-      <div className="portfolio-content">
-        <div id="hero"><Hero /></div>
-        <div id="about"><About /></div>
-        <div id="journey"><Journey /></div>
-        <div id="skills"><Skills /></div>
-        <div id="strengths"><Strengths /></div>
-        <div id="projects"><Projects /></div>
-        <div id="achievements"><Achievements /></div>
-        <div id="beyond"><Beyond /></div>
-        <div id="goals"><Goals /></div>
-        <div id="contact"><Contact /></div>
-      </div>
-    </div>
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<AnimatedSlide variant="scaleFade"><Hero /></AnimatedSlide>} />
+      <Route path="/about" element={<AnimatedSlide variant="slideLeft"><About /></AnimatedSlide>} />
+      <Route path="/strengths" element={<AnimatedSlide variant="blurFade"><Strengths /></AnimatedSlide>} />
+      <Route path="/contact" element={<AnimatedSlide variant="fadeSlide"><Contact /></AnimatedSlide>} />
+    </Routes>
   )
 }
 
